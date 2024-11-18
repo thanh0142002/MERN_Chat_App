e7dc27164c7a3956b13697ae5da614459e195e31
thanh0142002 <thanh0142002@gmail.com>
2024-11-18 21:17:05 +0700

    updata MongoDB

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://mern-chat-app-dpyn.onrender.com", {
+      const socket = io("https://mern-chat-app-9ptd.onrender.com", {
         query: {
           userId: authUser._id,
         },
